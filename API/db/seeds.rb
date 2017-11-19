5fa4b6ff0f73dc31152dacb4452149880e25f866
RyoYamada <newfield1001@gmail.com>
2017-11-19 16:10:57 +0900

    api: TodoItemを追加

--- API/db/seeds.rb
+++ API/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+TodoItem.create(:title => 'test_todo_1')
+TodoItem.create(:title => 'test_todo_2')
+TodoItem.create(:title => 'test_todo_3')
