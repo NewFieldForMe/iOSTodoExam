1d5ad8c50f1326444ecfdf412cd288f76f56c76e
RyoYamada <newfield1001@gmail.com>
2017-11-20 20:27:37 +0900

    iOS: TodoListItemCellを追加

---
+++ iOS/TodoExam/UIParts/TodoListItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  TodoListItemCell.swift
+//  TodoExam
+//
+//  Created by 山田良 on 2017/11/20.
+//  Copyright © 2017年 Yamada.Ryo. All rights reserved.
+//
+
+import  UIKit
+
+class TodoListItemCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
+
--- iOS/TodoExam/ViewController/TodoListViewController.swift
+++ iOS/TodoExam/ViewController/TodoListViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 
-class TodoListViewController: UIViewController {
+class TodoListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var todoList: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let service = APIService()
         service.getTodoList()
+        
+        todoList.delegate = self
+        todoList.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +26,17 @@ class TodoListViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoListItemCell") as! TodoListItemCell
+        
+        cell.titleLabel.text = "hogehoge"
+        
+        return cell
+    }
 
 }
 
